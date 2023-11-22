2503ec41fc107c9ac02bd9155b1d7dfc7d3466e5
Jonatas Santos <jonatas_santos3161@hotmail.com>
2023-11-22 03:12:15 -0300

    update packages

--- requirements/checklists/http/post.md
+++ requirements/checklists/http/post.md
@@ -1,6 +1,7 @@
 # HTTP Post
 
 > ## Sucesso
+
 1. ✅ Request com verbo http correto (post)
 2. ✅ Passar nos headers o content type JSON
 3. ✅ Chamar request com body correto
@@ -8,6 +9,7 @@
 5. ✅ No content - 204 e resposta sem dados
 
 > ## Erros
+
 1. ✅ Bad request - 400
 2. ✅ Unauthorized - 401
 3. ✅ Forbidden - 403
@@ -15,10 +17,13 @@
 5. ✅ Internal server error - 500
 
 > ## Exceção - Status code diferente dos citados acima
+
 1. ✅ Internal server error - 500
 
 > ## Exceção - Http request deu alguma exceção
+
 1. ✅ Internal server error - 500
 
 > ## Exceção - Verbo http inválido
+
 1. ✅ Internal server error - 500
--- test/data/use_cases/remote_authentication_test.dart
+++ test/data/use_cases/remote_authentication_test.dart
@@ -1,38 +1,40 @@
 import 'package:faker/faker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 class RemoteAuthentication {
-  final HttpClient httpClient;
+  final HttpClientSpy httpClient;
   final String url;
 
   RemoteAuthentication({required this.httpClient, required this.url});
-  Future<void> auth() async {
+  Future<void>? auth() async {
     await httpClient.request(url: url, method: 'post');
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({@required String url, @required method});
+  Future<void> request({
+    required String? url,
+    required String method,
+  });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  RemoteAuthentication? sut;
-  HttpClientSpy? http;
-  String? url;
+  late RemoteAuthentication sut;
+  late HttpClientSpy httpClient;
+  late String url;
 
-  setUp(() {
-    http = HttpClientSpy();
-    url = faker.internet.httpUrl();
-    sut = RemoteAuthentication(httpClient: http!, url: url!);
+  setUpAll(() {
+    httpClient = HttpClientSpy();
+    url = 'google.com';
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
 
-  test('Should call HttpClient with correct URL', () async {
-    await sut!.auth();
+  test('Should call HttpClient with correct values', () async {
+    await sut.auth();
 
-    verify(http!.request(url: url!, method: 'post'));
+    verify(httpClient.request(url: url, method: 'post')).called(1);
   });
 }
